2e9ea1be3353200eb360368add6a1002ab135bed
tsukinonoroi <seytumerov.e.i21@gmail.com>
2022-11-09 20:03:53 +0300

    4.4

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+app.engine('ejs',require('ejs-locals'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -39,3 +40,6 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+
