79a35c3124e722c28d7c8e6c44ec1ce92ad10063
tsukinonoroi <seytumerov.e.i21@gmail.com>
2022-11-16 19:04:24 +0300

    5.3

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,41 @@
+
 var express = require('express');
 var router = express.Router();
 
-/*pa*/
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/*Default PA*/ 
 router.get('/PhantomAssassin', function(req, res, next) {
   res.render('phantom', {
       title: "Phantom Assassin",
       picture: "images/phantom1.png",
-      desc: "Phantom Assassin "
+      desc: "Phantom Assassin идёт в наступление, едва завидев свою жертву. Предварительно атаковав противника кинжалом, а затем мгновенно сблизившись с ним, Мортред легко уклоняется от атак и обрушивает на врага удар за ударом, любой из которых может оказаться роковым. "
   });
 });
 
-/*arcana*/
+/*Arcana*/ 
 router.get('/Arcana', function(req, res, next) {
   res.render('phantom', {
       title: "Phantom Assassin with Arcana-item",
       picture: "images/phantom_arcana.png",
-      desc: "Arcana-item"
+      desc: "Издав хриплый смешок, старший кузнец Крейлер взмахнул мечом, ковавшимся 11 поколениями его семьи. Он был настолько острым, что окружающий мир, с треском разорвавшейся материи, разошелся. Через этот разрыв Крейлер увидел себя несколькими мгновениями ранее, державшего тот же самый вожделенный меч. Вдруг, в приступе алчности и сумасшествия, он сразил своего двойника, чтобы заполучить второй меч. Но, с запозданием Крейлер почувствовал знакомую рану, и внезапно воспоминания о ней нахлынули на него..."
   });
 });
 
-
-
+/*Phantom alt*/ 
+router.get('/Alternative', function(req, res, next) {
+  res.render('phantom', {
+      title: "Alternative Personality ",
+      picture: "images/alt_pers.png",
+      desc: "Асан-изгнанник, втайне взращённый отвергнутыми Сёстрами вуали, всю жизнь стремился к единственной цели: уничтожить ложного пророка, которого его учителя винили в развращении их когда-то святой веры. Одного за другим его стражей забирали кинжалы потаённых убийц, но теперь Асан наконец готов выйти из тени — чтобы исполнить тёмные пожелания своих наставников и воплотить роковые пророчества, вселяющие в оракула ужас."
+  });
+});
 
 module.exports = router;
+
+
+
+
