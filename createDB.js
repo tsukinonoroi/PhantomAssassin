d0b3b409998aedbef3d58f3781d7cd89ca30c2b0
tsukinonoroi <seytumerov.e.i21@gmail.com>
2022-11-23 21:24:22 +0300

    6.3

--- createDB.js
+++ createDB.js
@@ -1,18 +1,19 @@
 var MongoClient = require('mongodb').MongoClient
 var data = require("./data.js").data
-const uri = "mongodb://127.0.0.1:27017"
+
+const uri = "mongodb://127.0.0.1:27017/"
 const client = new MongoClient(uri)
 async function run() {
-try {
-await client.connect();
-var database = client.db("phantomassassin");
-database.dropDatabase()
-database = client.db("phantomassassin");
-const heroes = database.collection("heroes");
-const result = await heroes.insertOne({name:"Фантомка"});
-console.log(`${result} documents were inserted`);
-} finally {
-await client.close();
-}
+    try  {
+        await client.connect();
+        var database = client.db("phantomassassin");
+        database.dropDatabase()
+        database = client.db("phantomassassin");
+        const heroes = database.collection("heroes");
+        const result = await heroes.insertMany(data);
+        console.log(`${result.insertedCount} documents were inserted`);
+    } finally {
+        await client.close();
+    }
 }
 run()
