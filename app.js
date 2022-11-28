1f049bd4517832e8412b38edc45c4b8d20e63047
tsukinonoroi <seytumerov.e.i21@gmail.com>
2022-11-28 20:07:12 +0300

    8.2

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/phantomassassin')
+var phantom = require('./routes/phantom');
 
 
 var indexRouter = require('./routes/index');
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/phantom', phantom);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
