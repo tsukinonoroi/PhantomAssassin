e85bf99470303f646e95a237c84633ba2e313386
tsukinonoroi <seytumerov.e.i21@gmail.com>
2022-12-14 22:52:59 +0300

    10.4

--- models/user.js
+++ models/user.js
@@ -2,36 +2,40 @@ var mongoose = require("mongoose")
 var crypto = require("crypto")
 
 var schemaUser = mongoose.Schema({
-    username:{
-        type: String,
-        unique: true,
-        required: true
-    },
-    hashedPassword:{
-        type: String,
-        required: true
-    },
-    salt:{
-        type: String,
-        required: true
-    },
-    created:{
-        type:Date,
-        default: Date.now
-    }
+   username:{
+       type: String,
+       unique: true,
+       required: true
+   },
+   hashedPassword:{
+       type: String,
+       required: true
+   },
+   salt:{
+       type: String,
+       required: true
+   },
+   created:{
+       type:Date,
+       default: Date.now
+   }
 })
 
+
 schemaUser.virtual("password").set(function(password){
-    this._purePassword = password
-    this.salt = Math.random() + ""
-    this.hashedPassword = this.encryptPassword(password)
+   this._purePassword = password
+   this.salt = Math.random() + ""
+   this.hashedPassword = this.encryptPassword(password)
 }).get(function(){
-    return this._purePassword
+   return this._purePassword
 })
 
 schemaUser.methods.encryptPassword = function(password){
-    return crypto.createHmac('sha1', this.salt).update(password).digest('hex')
+   return crypto.createHmac('sha1', this.salt).update(password).digest('hex')
 }
 
+schemaUser.methods.checkPassword = function(password){
+    return this.encryptPassword(password) === this.hashedPassword
+}
 
 module.exports.User = mongoose.model("User",schemaUser)
