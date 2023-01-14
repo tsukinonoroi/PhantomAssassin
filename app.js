cbc941e150a8733c454c39c06f239274879cfb65
tsukinonoroi <seytumerov.e.i21@gmail.com>
2023-01-14 23:21:19 +0300

    11.4 + 11.5

--- app.js
+++ app.js
@@ -1,80 +1,90 @@
 var createError = require('http-errors');
 var express = require('express');
+
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/phantomassassin')
+//var mongoose = require('mongoose')
+//mongoose.connect('mongodb://localhost/threecats')
+var mysql2 = require('mysql2/promise');
+
+
 var session = require("express-session")
-var phantoms = require('./routes/phantoms');
-var Phantom = require("../models/phantom").Phantom
+//var MongoStore = require('connect-mongo');
+var MySQLStore = require('express-mysql-session')(session);
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var phantomsRouter = require('./routes/phantoms');
+
 
 var app = express();
+
+
+var options = {
+host : '127.0.0.1',
+port: '3306',
+user : 'root',
+password : 'root',
+database: 'phantomassassin'
+};
+var connection = mysql2.createPool(options)
+var sessionStore = new MySQLStore( options, connection);
+
 
 // view engine setup
 app.engine('ejs',require('ejs-locals'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bower_components')));
 
-var MongoStore = require('connect-mongo'); (session);
+
+// app.use(session({
+// secret: "ThreeCats",
+// cookie:{maxAge:60*1000},
+// store: MongoStore.create({mongoUrl: 'mongodb://localhost/threecats'})
+// }))
+
+
 app.use(session({
-  secret: "PhantomAssassin",
-  cookie:{maxAge:60*1000},
-  resave: true,
-  saveUninitialized: true,
-  store: MongoStore.create({mongoUrl: 'mongodb://127.0.0.1:27017/phantomassassin'})
-}))
-app.use(function(req,res,next){
-  req.session.counter = req.session.counter +1 || 1
-  next()
-})
+secret: 'PhantomAssassin',
+key: 'sid',
+store: sessionStore,
+resave: true,
+saveUninitialized: true,
+cookie: { path: '/',
+httpOnly: true,
+maxAge: 60*1000
+}
+}));
 
-app.use(function(req,res,next){
-  res.locals.nav = []
+var User = require("./../models/User").User
 
-  Cat.find(null,{_id:0,title:1,nick:1},function(err,result){
-      if(err) throw err
-      res.locals.nav = result
-      next()
-  })
-})
 
-app.use(require("./middleware/createMenu.js"))
-app.use(require("./middleware/createUser.js"))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/phantoms', phantoms);
+module.exports = function(req,res,next) {
+res.locals.user = null
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// User.findById(req.session.user, function (err, user) {
+// if (err)
+// return next(err)
+// res.locals.user = user;
+// next();
+// })
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error',
-  {
-    picture: "../images/error.png",
-    title: 'Ошибка, проверьте корректность запроса.'
-  });
-});
 
-module.exports = app;
+next();
 
+
+}
 
